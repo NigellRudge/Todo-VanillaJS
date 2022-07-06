62f44ebf7e47f2969a094195ceca60157d28a7f8
Deyon Rudge <deyon730@gmail.com>
2022-07-06 12:21:53 -0300

    ReadMe file

---
+++ README.md
@@ -0,0 +1,7 @@
+## Vanilla JS Todo Application
+
+A todo application built with:
+
+- Vanilla Javascript
+- CSS3
+- [Redux](https://redux.js.org/)
---
+++ index.js
@@ -0,0 +1,15 @@
+function addToStore(todoItem){
+    
+}
+
+function removeFromStore(itemId){
+
+}
+
+function completeItem(itemId){
+
+}
+
+function onAddClick($event){
+
+}
