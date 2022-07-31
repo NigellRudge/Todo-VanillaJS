611c923f0a73666715bfc310db48b7bfe9bcb884
Deyon Rudge <deyon730@gmail.com>
2022-07-30 21:33:50 -0300

    Keyboard events

--- index.js
+++ index.js
@@ -3,26 +3,25 @@ const STATUS_TYPES = {
     COMPLETE:'Complete'
 };
 
-let store = {
+var store = {
     activeFilter: null,
     categories:[
 
     ],
     todo: [
-        
     ]
 }
 
 
 const PENDING_ICON = `
-<svg id="pending-icon" onclick="completeItem(event)" xmlns="http://www.w3.org/2000/svg" width="24" height="24"  fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+<svg id="pending-icon" onclick="toggleItem(event)" xmlns="http://www.w3.org/2000/svg" width="24" height="24"  fill="currentColor" class="bi bi-check-circle status-icon" viewBox="0 0 16 16">
     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
     <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
 </svg>
 `;
 
 const COMPLETE_ICON = `
-<svg  onclick="setItemToPending(event)" id="complete-icon" xmlns="http://www.w3.org/2000/svg"  width="24" height="24"   fill="green" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+<svg  onclick="toggleItem(event)" id="complete-icon" xmlns="http://www.w3.org/2000/svg"  width="24" height="24"   fill="green" class="bi bi-check-circle-fill status-icon" viewBox="0 0 16 16">
     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
 </svg>
 
@@ -61,7 +60,9 @@ function getRandomColor(){
         '#5280ff',
         '#be51fc',
         '#ffab4a',
-        'rgb(250, 216, 21)'
+        'rgb(250, 216, 21)',
+        '#000',
+        '#fff',
     ];
     return colors[Math.floor(Math.random() * (colors.length-1))]
 }
@@ -83,7 +84,7 @@ function createTodoNode({id,title,status, color}){
                     <span class="item-title">${title}</span>
                     <div class="item-info">
                         Status: 
-                        <span id="item-status" class="pending">${status}</span>
+                        <span id="item-status" class="${status == STATUS_TYPES.PENDING? 'pending' : 'complete'}">${status}</span>
                     </div>
                 </div>
                 <div class="item-action-container">
@@ -121,52 +122,63 @@ function setUpFilterEvents(){
             removeClassFromNodes('filter', 'active');
             node.classList.add('active');
         })
+        node.classList.remove('active');
+    }
+    switch(store.activeFilter){
+        case null:{
+            let filterElement = document.getElementById('filter-all');
+            filterElement.classList.add('active');
+            return;
+        }
+        case STATUS_TYPES.PENDING:{
+            let filterElement = document.getElementById('filter-pending');
+            filterElement.classList.add('active');
+            return;
+        }
+        case STATUS_TYPES.COMPLETE:{
+            let filterElement = document.getElementById('filter-complete');
+            filterElement.classList.add('active');
+            return;
+        }
     }
 }
 
-function completeItem(event){
+function toggleItem(event){
     let target = event.target;
-    while(target.id !== 'pending-icon'){
-        target = target.parentElement
-    }
-    let parentNode = getParentNodeWithClass(target,'list-item');
-    let todoId = parentNode.getAttribute('todo-id');
-    let status = parentNode.querySelectorAll('.item-content')[0]
-                    .querySelectorAll('.item-info')[0]
-                    .querySelectorAll('#item-status')[0];
-    
-    let actionContainer =  parentNode.querySelectorAll('.item-action-container')[0]
-                        .querySelectorAll('.item-action')[0];
-
-    status.classList.remove('pending');
-    status.classList.add('complete');
-    status.innerHTML = 'Complete';
-    let todoItem = getTodoItem(todoId);
-    todoItem.status = STATUS_TYPES.COMPLETE;
-    actionContainer.innerHTML = COMPLETE_ICON;
-
-}
-
-function setItemToPending(event){
-    event.stopPropagation();
-    let target = event.target;
-    let parentNode = getParentNodeWithClass(target,'list-item');
-    let todoId = parentNode.getAttribute('todo-id');
-    let status = parentNode.querySelectorAll('.item-content')[0]
-                    .querySelectorAll('.item-info')[0]
-                    .querySelectorAll('#item-status')[0]
-    let actionContainer =  parentNode.querySelectorAll('.item-action-container')[0]
-                        .querySelectorAll('.item-action')[0];
-    status.classList.remove('complete');
-    status.classList.add('pending');
-    status.innerHTML = 'pending';
-    let todoItem = getTodoItem(todoId);
-    todoItem.status = STATUS_TYPES.PENDING;
-    actionContainer.innerHTML = PENDING_ICON;
+    if(target !== null){
+        while(!target.classList.contains('status-icon')){
+            target = target.parentElement
+        }    
+        let parentNode = getParentNodeWithClass(target,'list-item');
+        let todoId = parentNode.getAttribute('todo-id');
+        let status = parentNode.querySelectorAll('.item-content')[0]
+                        .querySelectorAll('.item-info')[0]
+                        .querySelectorAll('#item-status')[0];
+        let actionContainer =  parentNode.querySelectorAll('.item-action-container')[0]
+                            .querySelectorAll('.item-action')[0];
+        let todoItem = getTodoItem(todoId);
+        if(todoItem.status == STATUS_TYPES.PENDING){
+            status.classList.remove('pending');
+            status.classList.add('complete');
+            status.innerHTML = 'Complete';
+            
+            todoItem.status = STATUS_TYPES.COMPLETE;
+            actionContainer.innerHTML = COMPLETE_ICON;
+        }
+        else{
+            status.classList.remove('complete');
+            status.classList.add('pending');
+            status.innerHTML = 'pending';
+            let todoItem = getTodoItem(todoId);
+            todoItem.status = STATUS_TYPES.PENDING;
+            actionContainer.innerHTML = PENDING_ICON;
+        }      
+        populateList();
+    } 
 }
 
 
-function populateList(event){
+function populateList(){
     const todoList = document.getElementById('list');
     const deleteContainer = document.getElementById('delete-container');
     todoList.innerHTML = '';
@@ -182,6 +194,7 @@ function populateList(event){
         childNode.innerHTML = createTodoNode(item);
         todoList.appendChild(childNode);
     }
+    setUpFilterEvents();
 
 }
 
@@ -199,7 +212,7 @@ function deleteItem(event){
 }
 
 function addTodo(event){
-    let button = event.target.classList.contains('button-text')? event.target.parentElement: event.target;    
+    let button = document.getElementById('submitBtn') 
     let input = document.getElementById('title-input')
     if(input.value.length == 0){
         return;
@@ -218,10 +231,11 @@ function addTodo(event){
 
 }
 
+
 function onTextChange(event){
     let target = event.target;
     const button = document.getElementById('submitBtn')
-    if(target.value.length > 4){
+    if(target.value.length >= 4){
         button.classList.remove('disabled');
         button.removeAttribute('disabled');
     }
@@ -239,10 +253,67 @@ function deleteAllItems(){
     store.todo = [];
     populateList();
 }
+function captureKeyPress(event){
+    if(event.keyCode == 13){
+        let input = event.target;
+        let value = input.value;
+        if(value!== null && value!== '' && value.length > 4){
+            addTodo()
+        }
+    }
+    else if(event.keyCode == 27 ){
+        clearInput()
+    }
+}
 
-document.addEventListener('DOMContentLoaded', function(event){
-    setUpFilterEvents();
+function clearInput(){
+    let button = document.getElementById('submitBtn');
+    let input = document.getElementById('title-input');
+    input.value = '';
+    button.setAttribute('disabled','');
+    button.classList.add('disabled');
+}
+
+function switchFilterOnKeypress(event){
+    let key = event.key;
+        if(key == 'ArrowLeft' || key == 'ArrowRight'){
+            if(key == 'ArrowLeft'){
+                switch(store.activeFilter){
+                    case null:{
+                        return;
+                    }
+                    case STATUS_TYPES.PENDING:{
+                        onFilterClick(null);
+                        return;
+                    }
+                    case STATUS_TYPES.COMPLETE:{
+                        onFilterClick(STATUS_TYPES.PENDING);
+                        return;
+                    }
+                }
+            }
+            else if(key == 'ArrowRight'){
+                switch(store.activeFilter){
+                    case null:{
+                        onFilterClick(STATUS_TYPES.PENDING);
+                        return;
+                    }
+                    case STATUS_TYPES.PENDING:{
+                        onFilterClick(STATUS_TYPES.COMPLETE);
+                        return;
+                    }
+                    case STATUS_TYPES.COMPLETE:{
+                        return;
+                    }
+                }
+            }
+        } 
+}
+
+
+document.addEventListener('DOMContentLoaded', function(){
     populateList();
 
+    document.addEventListener('keydown',switchFilterOnKeypress);
     
 })
